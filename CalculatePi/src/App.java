aaf6edd553f1e5f0f9bd9e83f5d6b6be96aae8e2
tomeng70 <tomeng70@gmail.com>
2023-06-19 13:09:51 -0400

    Added some more comments and also track number of iterations it takes to calculate Pi.  Also added a new project called TileExample that demonstrates an algorithm to tile the edges of a room as well as the center of the room.

--- CalculatePi/src/App.java
+++ CalculatePi/src/App.java
@@ -12,13 +12,14 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         // initialize variables.
-        int sign = -1; // keep track of whether we're subtracting or adding.
-        double previousVal = 0;
-        double currentVal = 1;
-        double error = Math.abs(currentVal - previousVal);
+        int sign = -1;              // keep track of whether we're subtracting or adding.
+        double previousVal = 0;     // previous value in the series calculation.
+        double currentVal = 1;      // current value in the series calculation.
+        double error = Math.abs(currentVal - previousVal);      // calculate the error (as an abs val).
 
         // loop until error is below threshold value.
         int index = 3;
+        int steps = 0;
         while (error > THRESHOLD) {
             // update the previous value.
             previousVal = currentVal;
@@ -29,7 +30,7 @@ public class App {
             // cacluate the error (as an absolute value).
             error = Math.abs(currentVal - previousVal);
 
-            // display info for user.
+            // display debug info for user.
             if (DEBUG) {
                 System.out.println(String.format(" index = %04d, prev = %.12f, curr = %.12f, err = %.12f", index,
                     previousVal, currentVal, error));
@@ -40,13 +41,16 @@ public class App {
 
             // increase the index by two (we only want the odd numbers).
             index = index + 2;
+
+            // keep track of how many steps it took to solve this problem.
+            steps = steps + 1;
         }
         
         // calculate Pi.
         double pi = currentVal * 4;
 
         // display formatted output.
-        System.out.println(String.format("  index = %04d, prev = %.08f, curr = %.08f, err = %.08f", index, previousVal, currentVal, error));
+        System.out.println(String.format("  steps = %04d, prev = %.08f, curr = %.08f, err = %.08f", steps, previousVal, currentVal, error));
         System.out.println(String.format("  Pi = %.08f", pi));
     }
 }
---
+++ TileExample/src/App.java
@@ -0,0 +1,65 @@
+// This example program places "tiles" along the edges of the room.
+// It also places a few tiles in the center of the room.
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // set up a Scanner to get user input.
+        Scanner input = new Scanner(System.in);
+
+        // get dimensions of the room.
+        System.out.print("Enter the width of the room: ");
+        int width = input.nextInt();
+        System.out.print("Enter the height of the room: ");
+        int height = input.nextInt();
+
+        // calculate the special rows.
+        int y1, y2;
+        if (height % 2 == 1) {
+            // it's odd.
+            y1 = height / 2;
+
+            // set y2 to an out of range value.
+            y2 = -1;
+        } else {
+            // it's even.
+            y1 = height / 2 - 1;
+            y2 = y1 + 1;
+        }
+
+        // calculate the special columns.
+        int x1, x2, x3;
+        if (width % 2 == 1) {
+            // it's odd.
+            x2 = width / 2;
+            x1 = x2 - 1;
+            x3 = x2 + 1;
+        } else {
+            // it's even.
+            x1 = width / 2 - 1;
+            x2 = x1 + 1;
+            x3 = -1;
+        }
+
+        // go row by row.
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // if we're on an edge print "*"
+                // otherwise print " "
+                if (y == 0 || y == (height - 1)) {
+                    System.out.print("*");
+                } else if (x == 0 || x == (width - 1)) {
+                    System.out.print("*");
+                } else if ((y == y1 || y == y2) && (x == x1 || x == x2 || x == x3)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Hello, World!");
+    }
+}
