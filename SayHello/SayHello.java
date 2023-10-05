a103d297efaf32fb8c732d73c1650323882e52d4
tomeng70 <tomeng70@users.noreply.github.com>
2023-10-05 08:33:55 -0400

    Rm-ed some folders

--- SayHello/SayHello.java
+++
@@ -1,5 +0,0 @@
-public class SayHello {
-    public static void main(String[] args) {
-        System.out.println("Say hello!");
-    }
-}
--- Solutions/ScratchWork.java
+++ Solutions/ScratchWork.java
@@ -2,31 +2,18 @@ import java.util.Scanner;
 
 public class ScratchWork {
     static public void main(String args[]) {
-        String answer = String.valueOf(Math.PI / 2.0);
-        System.out.println("answer = " + answer);
-
-        String msg = String.format("PI = %.5f", Math.PI);
-        System.out.println(msg.length());
-
-        /*
-        // prompt user for age.
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = input.nextInt();
-        if (age = 13) {
-            System.out.println("Lucky 13!");
-        }
-        */
-        double a = 1.2; 
-        double b= 3.0;
-        double c = a * b; 
-        if(c == 3.6){
-            System.out.println("c is 3.6");
-        } else {
-            System.out.println("c is not 3.6");
-        } 
-        
 
+        /* 
+        // prompt user for name.
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        // print name.
+        System.out.println("name equals " + name);
+
+        */
+        System.out.println("This is a double quote mark: \"");
         
     }
 }
